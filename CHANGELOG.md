e34b1592112ecd9c20b35eea2912fe771a639753
liub <liub@getui.com>
2017-01-07 00:56:25 +0800

    add icon and gif

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@ All notable changes to the "shell-format" extension will be documented in this f
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
+## [0.1.2] - 2017-01-07
+- add icon & gif
+
 ## [0.1.1] - 2017-01-06
 - fix document
 
--- test/test.sh
+++ test/test.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 COUNTER=0
-while [ $COUNTER -lt 5 ]
-do
-    COUNTER=`expr $COUNTER + 1`
-    echo $COUNTER
+while [ $COUNTER -lt 5 ]; do
+	COUNTER=$(expr $COUNTER + 1)
+	echo $COUNTER
 done
 
 echo 'type <CTRL-D> to terminate'
 echo -n 'enter your most liked film: '
-while read FILM
-do
-         echo "Yeah! great film the $FILM"
+while read FILM; do
+	echo "Yeah! great film the $FILM"
 done
 
-
 a=0
-until [ ! $a -lt 10 ]
-do
-    echo $a
-   a=`expr $a + 1`
+until [ ! $a -lt 10 ]; do
+	echo $a
+	a=$(expr $a + 1)
+	if [ $a -eq 3 ]; then
+		echo "yeah i am three"
+	fi
 done
