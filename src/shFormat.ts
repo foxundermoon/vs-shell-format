21cc9d642bfd453b7219f924d8e003defe2cf35b
fox <fox@vvfox.com>
2019-01-17 15:27:06 +0800

    add doc ,adapter active events

--- src/shFormat.ts
+++ src/shFormat.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 import * as cp from "child_process";
 import * as path from "path";
-import * as chalk from "chalk";
 import { fileExists, getExecutableFileUnderPath } from "./pathUtil";
 import {
   isDiffToolAvailable,
