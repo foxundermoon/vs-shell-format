ce7eb8f7a27e45d5109a80c244433d808b64d621
fox <fox@vvfox.com>
2019-09-20 18:39:50 +0800

    fix test

--- test/shfmt.test.ts
+++ test/shfmt.test.ts
@@ -1,13 +1,14 @@
-import { getSettings } from '../src/shFormat'
-import * as assert from 'assert'
-import { fileExists } from '../src/pathUtil';
+import { getSettings } from "../src/shFormat";
+import * as assert from "assert";
+import { fileExists } from "../src/pathUtil";
 
 suite("shfmt path Tests", () => {
+  let shfmtPath = getSettings("path");
 
-    let shfmtPath = getSettings("path");
-
-    // Defines a Mocha unit test
+  // Defines a Mocha unit test
+  if (shfmtPath) {
     test("shfmt exists", () => {
-        assert.equal(fileExists(shfmtPath), true);
+      assert.equal(fileExists(shfmtPath), true);
     });
+  }
 });
